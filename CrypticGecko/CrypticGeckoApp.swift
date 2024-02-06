bc63bc938a4278207683b814e4d58f2a75fb6cf1
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-02-06 18:08:31 +0100

    Initial Commit

---
+++ CrypticGecko/CrypticGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypticGeckoApp.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 06.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CrypticGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
