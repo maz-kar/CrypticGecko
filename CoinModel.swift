9f8a13cb1a7b172ceb7554f0adbf62e475064664
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-24 14:16:49 +0100

    coinModel was added.

---
+++ CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 23.03.24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CoinModel: Codable {
+    var id: String
+    
+}
--- Home/HomeView.swift
+++ Home/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 class HomeViewModel: ObservableObject { //TODO: Move it back to its folder
     
-    @Published var headerText: String = ""
+    //@Published var headerText: String = "" //TODO: CHECK
     
 }
 
@@ -22,7 +22,6 @@ struct HomeView: View {
         ZStack {
             Color.theme.backgroundColor
                 .ignoresSafeArea()
-            
             VStack {
                 HStack(spacing: 80) {
                     Button(action: {
@@ -41,10 +40,59 @@ struct HomeView: View {
                             chevronRightPressed.toggle()
                         }
                 }
+                .padding()
+                
+                VStack {
+                    HStack(spacing: 40) { //TODO: fix the space
+                        Text("Market Cap")
+                            .foregroundStyle(Color.theme.secondaryTextColor) //TODO: Refactor the repeated code
+                            .font(.headline)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                        Text("24h Volume") //TODO: Put this in the middle, aligned with Live Prices
+                            .foregroundStyle(Color.theme.secondaryTextColor)
+                            .font(.headline)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                        Text("BTC Dominance") //TODO: instead of BTC must be dynamic
+                            .foregroundStyle(Color.theme.secondaryTextColor)
+                            .font(.headline)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                    }
+                }
+                VStack {
+                    HStack(spacing: 70) { //TODO: fix the space
+                        //TODO: Fix the position of the text, to start from left
+                        Text("$1.35Tr")
+                            .foregroundStyle(Color.theme.accentColor) //TODO: Refactor the repeated code
+                            .font(.headline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+
+                        Text("$262.24Bn") //TODO: Put this in the middle, aligned with Live Prices
+                            .foregroundStyle(Color.theme.accentColor)
+                            .font(.headline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Text("45.21%") //TODO: instead of BTC must be dynamic
+                            .foregroundStyle(Color.theme.accentColor)
+                            .font(.headline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                }
+                VStack {
+                    HStack(spacing: 70) {
+                        Text("-16.08%") //TODO: Add arrow down/up symbol
+                        //TODO: shift the text to the left
+                        
+                            .foregroundStyle(Color.theme.redColor)
+                            .font(.footnote)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                    }
+                }
                 Spacer()
             }
         }
-//        .onAppear(perform: {})
     }
 }
 
@@ -53,13 +101,13 @@ struct HomeView: View {
 }
 
 /*
-TODO:
-Create the navigationView and hide its bar
-add onTapGesture withAnimation
-add rotationEffect for the chevron.right instead of chevron.left
-use animation none on plus info
-add animation for the circleButton
-Refactor the home view in the end
+ TODO:
+ Create the navigationView and hide its bar
+ add onTapGesture withAnimation
+ add rotationEffect for the chevron.right instead of chevron.left
+ use animation none on plus info
+ add animation for the circleButton
+ Refactor the home view in the end
  
-DONE:
+ DONE:
  */
