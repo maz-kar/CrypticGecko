bac14b60dfd9dc11165e1d181d6e54e7265bb86f
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-04-08 18:39:23 +0200

    PreviewProvider was extended.

---
+++ Home/CoinsRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinsRowView.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 08.04.24.
+//
+
+import SwiftUI
+
+struct CoinsRowView: View {
+    let coin: CoinsModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinsRowView(coin: dev.coin)
+    }
+}
