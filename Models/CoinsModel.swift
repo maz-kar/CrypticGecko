ae203a94c34b4bcf062b83bb92f360082dca7d28
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-04-08 17:38:32 +0200

    currentHoldings, updateHoldings, currentHoldingsValue and rank were added to the CoinsModel.

--- Models/CoinsModel.swift
+++ Models/CoinsModel.swift
@@ -65,6 +65,20 @@ struct CoinsModel: Identifiable, Codable {
     let lastUpdated: String?
     let sparklineIn7D: SparklineIn7D?
     let priceChangePercentage24HInCurrency: Double?
+    let currentHoldings: Double?
+    
+    func updateHoldings(amount: Double) -> CoinsModel {
+        return CoinsModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChange24H, currentHoldings: amount)
+        //when calling updateHoldings, it's gonna give us a new coinsModel with all the same data as the current coinsModel, but with the updatedHoldings
+    }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
 }
 
 struct SparklineIn7D: Codable {
