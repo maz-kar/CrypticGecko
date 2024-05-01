6a364fe9d3e724bec41b0a8586c7d18db24038c4
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-05-01 17:00:58 +0200

    Double's extension was modified to cover only 2 decimal places for specific cases.

--- Extensions/Double.swift
+++ Extensions/Double.swift
@@ -9,6 +9,32 @@ import Foundation
 
 extension Double {
     
+    ///Converts a Double into a Currency with 2 decimal places
+    ///```
+    ///Convert 1234.56 to $1,234.56
+    ///```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        
+        return formatter
+    }
+    
+    ///Converts a Double into a Currency as a String with 2 decimal places
+    ///```
+    ///Convert 1234.56 to "$1,234.56"
+    ///```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     ///Converts a Double into a Currency with 2-6 decimal places
     ///```
     ///Convert 1234.56 to $1,234.56
