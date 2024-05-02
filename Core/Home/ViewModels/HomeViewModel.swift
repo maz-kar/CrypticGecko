ec8c723a44fef12215240bcf72fa359b7c084c50
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-05-02 15:33:19 +0200

    fake download was simulated.

--- Core/Home/ViewModels/HomeViewModel.swift
+++ Core/Home/ViewModels/HomeViewModel.swift
@@ -10,4 +10,13 @@ import SwiftUI
  
 class HomeViewModel: ObservableObject {
     
+    @Published var allCoinsList: [CoinsModel] = []
+    @Published var portfolioCoinsList: [CoinsModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoinsList.append(DeveloperPreview.instance.coin)
+            self.portfolioCoinsList.append(DeveloperPreview.instance.coin)
+        }
+    }
 }
