a510743e3923471aeabb62c59e91f40e31be4a34
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-05-03 16:31:07 +0200

    coinDataService was added in order to take over the job from the ViewModel

---
+++ Services/CoinDataService.swift
@@ -0,0 +1,45 @@
+//
+//  CoinDataService.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 03.05.24.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinsModel] = []
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        let decoderData = JSONDecoder()
+        decoderData.keyDecodingStrategy = .convertFromSnakeCase
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en") else { return }
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard
+                    let response = output.response as? HTTPURLResponse,
+                    response.statusCode >= 200 && response.statusCode < 300 else { throw URLError(.badServerResponse) }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinsModel].self, decoder: decoderData)
+            .sink { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error): print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] returnedData in
+                self?.allCoins = returnedData
+                self?.coinSubscription?.cancel()
+            }
+    }
+    
+}
