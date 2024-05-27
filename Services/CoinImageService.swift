24f132b67a1d9a212302462b6a7f09ab515f6344
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-05-27 08:32:08 +0200

    Testing class was deleted.

--- Services/CoinImageService.swift
+++ Services/CoinImageService.swift
@@ -14,20 +14,12 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinsModel
-    private let fileManager = LocalFileManager.instance
     
     init(coin: CoinsModel) {
         self.coin = coin
-        getCoinImage()
-    }
-    
-    private func getCoinImage() {
-        //TODO: if can get it from fileManager do it, else download it
-        //fileManager.getImage
     }
     
     private func downloadCoinImage() {
-        print("Downloading Image")
         guard let url = URL(string: coin.image) else { return }
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
@@ -37,9 +29,6 @@ class CoinImageService {
                 guard let self = self else { return }
                 self.image = returnedImage
                 self.imageSubscription?.cancel()
-                //TODO: save the image here
-                //fileManager.saveImage
-                
             })
     }
 }
