154c35ac374eee432d06d4b7012666efde2774be
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-07-03 17:02:46 +0200

    Test struct was added. Redundant ContentView was deleted.

---
+++ Core/Home/Views/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 03.07.24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
