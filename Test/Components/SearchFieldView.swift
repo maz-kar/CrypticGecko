b8de1ba9887de3ecb263972cc311e86185d09c75
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-07-04 17:17:38 +0200

    SearchFieldView was separated on its own struct.

---
+++ Test/Components/SearchFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchFieldView.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 04.07.24.
+//
+
+import SwiftUI
+
+struct SearchFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchFieldView()
+}
