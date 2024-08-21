62ae6a0fabf2e99d707582ae3ca8f5813c1a87c4
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-08-21 22:03:59 +0200

    Dummy data for preview created.

--- Extensions/PreviewProvider.swift
+++ Extensions/PreviewProvider.swift
@@ -22,6 +22,10 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let stat1 = StatisticModel(title: "Market Place", value: "$12.5Bn", precentageChange: 25.34)
+    let stat2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
+    let stat3 = StatisticModel(title: "Portfolio Value", value: "$50.4k", precentageChange: -12.34)
+    
     let coin = CoinsModel(
        id: "bitcoin",
        symbol: "btc",
