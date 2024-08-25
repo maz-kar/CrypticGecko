8f1296ceb7707bcb88bcd2c71a63c86ef461504b
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-08-25 20:14:45 +0200

    CoinLogoView created.

---
+++ Core/Home/Views/CoinLogoView.swift
@@ -0,0 +1,50 @@
+//
+//  CoinLogoView.swift
+//  CrypticGecko
+//
+//  Created by Maziar Layeghkar on 25.08.24.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    let coin: CoinsModel
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CoinImageView(coin: coin)
+                .frame(width: 50, height: 50)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.accentColor)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            
+            Text(coin.name)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryTextColor)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+
+        }
+        
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinLogoView(coin: dev.coin)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.light)
+            
+            CoinLogoView(coin: dev.coin)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+
+        
+    }
+}
