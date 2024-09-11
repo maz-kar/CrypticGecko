c9f39fa7c6a7170dc35fb271aafba1519671ddc4
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-11 14:57:38 +0200

    CoinDetailDataService was created.

--- Services/CoinDetailDataService.swift
+++ Services/CoinDetailDataService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Combine
+
+class CoinDetailDataService {
+    let coin: CoinsModel
+    
+    @Published var coinDetails: CoinDetailModel? = nil
+    var coinDetailSubscription: AnyCancellable?
+    
+    init(coin: CoinsModel) {
+        self.coin = coin
+        getCoinDetails(coin: coin)
+    }
+    
+    func getCoinDetails(coin: CoinsModel) {
+        let decoderData = JSONDecoder()
+        decoderData.keyDecodingStrategy = .convertFromSnakeCase
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false") else { return }
+        coinDetailSubscription = NetworkingManager.download(url: url)
+            .decode(type: CoinDetailModel.self, decoder: decoderData)
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoinDetail in
+                self?.coinDetails = returnedCoinDetail
+                self?.coinDetailSubscription?.cancel()
+            })
+    }
+    
+}
