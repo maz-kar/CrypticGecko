32c3a4af9a07c60641998c1b69bd19003bfbc29a
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-12 13:45:35 +0200

    lineColor was dependent on the priceChange.

--- Core/Detail/Views/ChartView.swift
+++ Core/Detail/Views/ChartView.swift
@@ -11,39 +11,46 @@ struct ChartView: View {
     let data: [Double]
     let minY: Double
     let maxY: Double
+    let lineColor: Color
     
     init(coin: CoinModel) {
         self.data = coin.sparklineIn7D?.price ?? []
         self.minY = data.min() ?? 0
         self.maxY = data.max() ?? 0
+        
+        //To check if the graph is a + or -
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        self.lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
     }
     
     var body: some View {
         GeometryReader { geometry in
-            Path { path in
-                //loop through the index of coins
-                for index in data.indices {
-                    /*
-                    if full width = 300
-                    if data.count = 100 --> 300/100 = 3
-                    0+1 * 3 = 3
-                    99+1 * 3 = 300
-                    so our x will go all the way from 3 to 300
-                    */
-                    let xPosition = (geometry.size.width / CGFloat(data.count)) * CGFloat(index + 1)
-                    
-                    let yAxis = maxY - minY
-                    let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) * geometry.size.height
-                    
-                    
-                    //if we are in the 1st loop, let's move the pointer to where we want to start
-                    if index == 0 {
-                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+            VStack() {
+                Path { path in
+                    //loop through the index of coins
+                    for index in data.indices {
+                        /*
+                         if full width = 300
+                         if data.count = 100 --> 300/100 = 3
+                         0+1 * 3 = 3
+                         99+1 * 3 = 300
+                         so our x will go all the way from 3 to 300
+                         */
+                        let xPosition = (geometry.size.width / CGFloat(data.count)) * CGFloat(index + 1)
+                        
+                        let yAxis = maxY - minY
+                        let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) * geometry.size.height
+                        
+                        
+                        //if we are in the 1st loop, let's move the pointer to where we want to start
+                        if index == 0 {
+                            path.move(to: CGPoint(x: xPosition, y: yPosition)) //starting point is now the first x and y position
+                        }
+                        path.addLine(to: CGPoint(x: xPosition, y: yPosition))
                     }
-                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
                 }
+                .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
             }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
         }
     }
 }
@@ -52,7 +59,7 @@ struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             ChartView(coin: dev.coin)
-                //.frame(width: 200)
+            //.frame(width: 200)
         }
     }
 }
