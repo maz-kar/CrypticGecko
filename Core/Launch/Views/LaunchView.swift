edf82ef3aa0ba4b3ef9376e1d98d709cdfc51e16
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-13 12:35:35 +0200

    loadingText was added.

--- Core/Launch/Views/LaunchView.swift
+++ Core/Launch/Views/LaunchView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var showLaunchView: Bool = false
+    
     var body: some View {
         ZStack {
             Color.launch.background
@@ -16,6 +19,20 @@ struct LaunchView: View {
             Image("logo-transparent")
                 .resizable()
                 .frame(width: 100, height: 100)
+            
+            ZStack {
+                if showLaunchView {
+                    Text(loadingText)
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(Color.launch.accent)
+                        .transition(AnyTransition.scale.animation(.easeIn))
+                }
+            }
+            .offset(y: 70)
+        }
+        .onAppear {
+            showLaunchView.toggle()
         }
     }
 }
